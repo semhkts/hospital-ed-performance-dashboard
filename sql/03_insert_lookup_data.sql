b547c4dfedcb28af087b8750f03b948b6fcbcda2
semhkts <38770811+semhkts@users.noreply.github.com>
2026-07-21 20:34:19 +1000

    Add lookup table reference data

--- sql/03_insert_lookup_data.sql
+++ sql/03_insert_lookup_data.sql
@@ -6,10 +6,6 @@
 
 USE hospital_ed_dashboard;
 
--------------------------------------------------------------
--- TRIAGE CATEGORIES
--------------------------------------------------------------
-
 INSERT INTO triage_categories (
     triage_category_id,
     category_name,
@@ -18,14 +14,10 @@ INSERT INTO triage_categories (
 )
 VALUES
 (1, 'Category 1', 0, 'Immediate life-threatening condition'),
-(2, 'Category 2', 10, 'Imminently life-threatening'),
-(3, 'Category 3', 30, 'Potentially life-threatening'),
-(4, 'Category 4', 60, 'Potentially serious'),
-(5, 'Category 5', 120, 'Less urgent');
-
--------------------------------------------------------------
--- ARRIVAL METHODS
--------------------------------------------------------------
+(2, 'Category 2', 10, 'Imminently life-threatening condition'),
+(3, 'Category 3', 30, 'Potentially life-threatening condition'),
+(4, 'Category 4', 60, 'Potentially serious condition'),
+(5, 'Category 5', 120, 'Less urgent condition');
 
 INSERT INTO arrival_methods (
     arrival_method_id,
@@ -36,12 +28,8 @@ VALUES
 (2, 'Walk-in'),
 (3, 'GP Referral'),
 (4, 'Police'),
-(5, 'Transfer'),
+(5, 'Hospital Transfer'),
 (6, 'Helicopter');
-
--------------------------------------------------------------
--- DEPARTMENTS
--------------------------------------------------------------
 
 INSERT INTO departments (
     department_id,
@@ -54,3 +42,18 @@ VALUES
 (4, 'Mental Health'),
 (5, 'Paediatrics'),
 (6, 'Short Stay');
+
+INSERT INTO diagnosis_groups (
+    diagnosis_group_id,
+    diagnosis_group_name
+)
+VALUES
+(1, 'Injury and Trauma'),
+(2, 'Respiratory'),
+(3, 'Cardiac'),
+(4, 'Gastrointestinal'),
+(5, 'Neurological'),
+(6, 'Mental Health'),
+(7, 'Infection'),
+(8, 'Paediatric'),
+(9, 'Other');
